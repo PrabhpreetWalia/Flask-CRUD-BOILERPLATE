7406f6e06ba434f1f5b3a8363bcc5338e55d3b4c
Prabhpreet <124711964+PrabhpreetWalia@users.noreply.github.com>
2023-06-08 07:22:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "<h1> CRUD Flask </h1>"
+
+from controller import user_controller
+
+if __name__ == "__main__":
+    app.run(debug= True)
